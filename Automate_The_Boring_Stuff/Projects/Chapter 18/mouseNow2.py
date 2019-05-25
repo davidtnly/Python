c496779c592cffe42f598ebabce3dd3bf46f7f4d
David <davidtnly@gmail.com>
2019-05-25 12:24:12 -0700

    added 'as pag'

--- Automate_The_Boring_Stuff/Projects/Chapter 18/mouseNow2.py
+++ Automate_The_Boring_Stuff/Projects/Chapter 18/mouseNow2.py
@@ -1,7 +1,7 @@
 #! python3
 # mouseNow.py - Displays the mouse cursor's current position.
 
-import pyautogui
+import pyautogui as pag
 import time
 
 time_elapsed = 0
@@ -10,9 +10,9 @@ print('Press Ctrl-C to quit.')
 try:
     while True:
         # Get and print the mouse coordinates.
-        x, y = pyautogui.position()
+        x, y = pag.position()
         positionStr = 'X: ' + str(x).rjust(4) + ' Y: ' + str(y).rjust(4)
-        pixelColor = pyautogui.screenshot().getpixel((x, y))
+        pixelColor = pag.screenshot().getpixel((x, y))
         positionStr += ' RGB: (' + str(pixelColor[0]).rjust(3)
         positionStr += ', ' + str(pixelColor[1]).rjust(3)
         positionStr += ', ' + str(pixelColor[2]).rjust(3) + ')'
