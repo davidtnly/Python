e768833db0ae5172ab9f31d7ba594556e27b2160
David <davidtnly@gmail.com>
2019-05-26 23:23:51 -0700

    added week 1 and 2 notes

---
+++ Learn_to_Program_The_Fundamentals/notes.py
@@ -0,0 +1,12 @@
+# def announce_location(country):
+# #     return country
+# #
+# #
+# # instructor_location = announce_location('canada')
+# #
+# # print(instructor_location)
+# #
+# # instructor_location  # does nothing
+
+x = None
+print(x)
