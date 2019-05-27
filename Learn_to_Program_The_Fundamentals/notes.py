5d025edf8bae2eadf1de98dd2199c21795697169
David <davidtnly@gmail.com>
2019-05-27 12:26:09 -0700

    added coursera assignments and notes

--- Learn_to_Program_The_Fundamentals/notes.py
+++ Learn_to_Program_The_Fundamentals/notes.py
@@ -10,3 +10,85 @@
 
 x = None
 print(x)
+
+digits = '0123456789'
+result = 100
+
+for digit in digits:
+    result = result - int(digit)
+
+print(result)
+
+
+digits = '0123456789'
+result = 0
+
+for digit in digits:
+    result = digit
+
+print(result)
+
+digits = '0123456789'
+result = ''
+
+for digit in digits:
+    result = result + digit * 2
+
+print(result)
+
+digits = '0123456789'
+result = ''
+
+for digit in digits:
+    result = result + digit * 2
+
+print(result)
+
+
+def secret(s):
+    i = 0
+    result = ''
+
+    while s[i].isdigit():
+        result = result + s[i]
+        i = i + 1
+
+    return result
+
+def example(L):
+    """ (list) -> list
+    """
+    i = 0
+    result = []
+    while i < len(L):
+        result.append(L[i])
+        i = i + 3
+    return result
+
+letters = ['b', 'd', 'a']
+letters.sort()
+print(letters)
+
+treats = [['apple', 'pie'], ['vanilla', 'ice-cream'], ['chocolate', 'cake']]
+print(treats[-3][-1])
+
+c = 0
+for i in range(2, 5):
+    for j in range(4, 9):
+        print(i, j)
+        c += 1
+        print(c)
+
+d = {'a': 1, 'c': 3, 'b': 2}
+not('e'in d)
+
+
+tup = (1,2,3)
+
+def f(x):
+    y = x * 3
+    return y - x
+
+start = 'L'
+middle = 8
+end = 'R'
